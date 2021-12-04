0c5f3a9a2ef12018be83ed54a9e2cc96e7d3473c
Zachary Fedorak <59974372+ZFedorak@users.noreply.github.com>
2021-12-03 18:10:35 -0700

    added github provider login in

--- components/authentication/providers/github/GitHubProvider.js
+++ components/authentication/providers/github/GitHubProvider.js
@@ -1,12 +1,31 @@
 import Image from "next/image";
-
+import { useRouter } from "next/router";
+import { GithubAuthProvider, signInWithPopup } from "@firebase/auth";
+import { useState } from "react";
+import {auth} from 'lib/firebase'
+import { useAuth } from "lib/hooks/useAuth";
 import { ProviderButton } from "ui/buttons";
 
 import github from "./github.png";
 
 function GitHubProvider({ children, ...props }) {
+  const user = useAuth();
+  const router = useRouter();
+  const [isValidUser, setIsValidUser] = useState(null)
+  const provider = new GithubAuthProvider()
+  
+
+  async function requestLogin(){
+    setIsValidUser(await signInWithPopup(auth, provider))
+  }
+  function handleClick(){
+    requestLogin()
+  }
+  if(isValidUser){
+    router.push('/todo')
+  }
   return (
-    <ProviderButton>
+    <ProviderButton onClick={handleClick} {... props} >
       <div>
         <Image
           src={github}
--- components/authentication/providers/google/GoogleProvider.js
+++ components/authentication/providers/google/GoogleProvider.js
@@ -1,12 +1,28 @@
 import Image from 'next/image'
 import { ProviderButton } from "ui/buttons";
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { useAuth } from 'lib/hooks/useAuth';
 import google from "./google.png";
-
+import {auth} from 'lib/firebase'
+import {GoogleAuthProvider, signInWithPopup} from '@firebase/auth'
 function GoogleProvider({ children,  ...props }) {
- 
+  const provider = new GoogleAuthProvider()
+  const router = useRouter()
+  const user = useAuth()
+  const [isValidUser, setIsValidUser] = useState(null)
+  async function requestLogin(){
+      setIsValidUser(await signInWithPopup(auth, provider))
+  }
+  function handleClick(){
+    requestLogin()
+  }
+  if(isValidUser){
+    router.push('/todo')
 
+  }
   return (
-    <ProviderButton >
+    <ProviderButton onClick={handleClick} {...props}>
       <div>
         <Image
           src={google}
